1549886feeedc1721e08c91083a65478d529c616
DU xing yu <2669512446@qq.com>
2026-06-10 14:06:47 +0800

    style(hwc): 格式化 HwcRequest.php 代码样式并改进错误处理
    - 统一函数定义的大括号格式
    - 修复数组参数中的多余逗号
    - 标准化条件语句的括号和空格格式
    - 优化字符串连接的空格格式
    - 将对账单下载失败时的直接输出改为异常抛出
    - 改进代码可读性和一致性

--- src/Hwc/HwcRequest.php
+++ src/Hwc/HwcRequest.php
@@ -80,11 +80,12 @@ class HwcRequest
     }
 
 
-    public function refund($params){
-        $this->reqHandler->setReqParams($params,['method']);
+    public function refund($params)
+    {
+        $this->reqHandler->setReqParams($params, ['method']);
         $reqParam = $this->reqHandler->getAllParameters();
-        if(empty($reqParam['transaction_id']) && empty($reqParam['out_trade_no'])){
-            return  ['status' => 500, 'msg' => '请输入商户订单号或平台订单号!'];
+        if (empty($reqParam['transaction_id']) && empty($reqParam['out_trade_no'])) {
+            return ['status' => 500, 'msg' => '请输入商户订单号或平台订单号!'];
         }
         $this->reqHandler->setParameter('version', $this->cfg['version']);
 
@@ -98,56 +99,58 @@ class HwcRequest
         $this->reqHandler->createSign();//创建签名
         $data = Utils::toXml($this->reqHandler->getAllParameters());//将提交参数转为xml，目前接口参数也只支持XML方式
 
-        $this->pay->setReqContent($this->reqHandler->getGateURL(),$data);
-        if($this->pay->call()){
+        $this->pay->setReqContent($this->reqHandler->getGateURL(), $data);
+        if ($this->pay->call()) {
             $this->resHandler->setContent($this->pay->getResContent());
             $this->resHandler->setKey($this->reqHandler->getKey());
-            if($this->resHandler->isTenpaySign()){
+            if ($this->resHandler->isTenpaySign()) {
                 //当返回状态与业务结果都为0时才返回支付二维码，其它结果请查看接口文档
-                if($this->resHandler->getParameter('status') == 0 && $this->resHandler->getParameter('result_code') == 0){
+                if ($this->resHandler->getParameter('status') == 0 && $this->resHandler->getParameter('result_code') == 0) {
                     return $this->resHandler->getAllParameters();
-                }else{
+                } else {
                     return ['status' => 500, 'msg' => $this->resHandler->getParameter('err_msg')];
                 }
             }
             return ['status' => 500, 'msg' => $this->resHandler->getParameter('message')];
-        }else{
+        } else {
             return ['status' => 500, 'msg' => $this->pay->getErrInfo()];
         }
     }
 
-    public function callbackQuery($xml){
+    public function callbackQuery($xml)
+    {
         $this->resHandler->setContent($xml);
         $data = [
             'service' => 'unified.trade.query',
-            'out_trade_no'=> $this->resHandler->getParameter('out_trade_no'),
+            'out_trade_no' => $this->resHandler->getParameter('out_trade_no'),
         ];
-        $response= $this ->execute($data);
-        if(!empty($response) && $response['status'] == 0 && $response['result_code'] == 0){
-            if ($response['trade_state'] == 'SUCCESS'){
+        $response = $this->execute($data);
+        if (!empty($response) && $response['status'] == 0 && $response['result_code'] == 0) {
+            if ($response['trade_state'] == 'SUCCESS') {
                 return $response;
             }
         }
         return ['status' => 500, 'msg' => $response];
     }
 
-    public function callback($xml){
+    public function callback($xml)
+    {
         $this->resHandler->setContent($xml);
 //        $this->resHandler->setKey($this->cfg->C('key'));
-        if($this->resHandler->isTenpaySign()){
-            if($this->resHandler->getParameter('status') == 0 && $this->resHandler->getParameter('result_code') == 0){
+        if ($this->resHandler->isTenpaySign()) {
+            if ($this->resHandler->getParameter('status') == 0 && $this->resHandler->getParameter('result_code') == 0) {
                 $tradeno = $this->resHandler->getParameter('out_trade_no');
                 // 此处可以在添加相关处理业务，校验通知参数中的商户订单号out_trade_no和金额total_fee是否和商户业务系统的单号和金额是否一致，一致后方可更新数据库表中的记录。
                 //更改订单状态
-                Utils::dataRecodes('接口回调收到通知参数',$this->resHandler->getAllParameters());
+                Utils::dataRecodes('接口回调收到通知参数', $this->resHandler->getAllParameters());
                 ob_clean();
                 return $this->resHandler->getAllParameter();
-            }else{
+            } else {
                 return ['status' => 500, 'msg' => $this->resHandler->getParameter('err_msg')];
             }
             return ['status' => 500, 'msg' => $this->resHandler->getParameter('message')];
-        }else{
-            return ['status' => 500, 'msg' => $this->pay->getErrInfo(),'message'=>'签名校验失败'];
+        } else {
+            return ['status' => 500, 'msg' => $this->pay->getErrInfo(), 'message' => '签名校验失败'];
         }
     }
 
@@ -163,14 +166,14 @@ class HwcRequest
         $this->reqHandler->createSign();//创建签名
         $data = Utils::toXml($this->reqHandler->getAllParameters());
 
-        $this->pay->setReqContent($this->reqHandler->getGateURL(),$data);
-        if($this->pay->callDown()){
+        $this->pay->setReqContent($this->reqHandler->getGateURL(), $data);
+        if ($this->pay->callDown()) {
             return $this->pay->getResContent();
 //            echo $content;
 //            $flag=Utils::dataRecodes( "对账单数据", $content);
 //            var_dump($flag);exit();
-        }else{
-            echo json_encode(array('status'=>500,'msg'=>'Response Code:'.$this->pay->getResponseCode().' Error Info:'.$this->pay->getErrInfo()));
+        } else {
+            throw new \Exception($this->pay->getErrInfo());
         }
     }
 
